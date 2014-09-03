2d8c7775d65ffc7c0e8a66419f404f09491c06ed
lehmamic <leh@zuehlke.com>
2014-09-03 21:37:23 +0200

    Remove unused method in the DeploymentManager

--- src/OpenDeploymentManager/OpenDeploymentManager.Deployment/DeploymentManager.cs
+++ src/OpenDeploymentManager/OpenDeploymentManager.Deployment/DeploymentManager.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Activities;
-using System.Activities.Expressions;
 using System.Activities.XamlIntegration;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 using Microsoft.Practices.ServiceLocation;
 using OpenDeploymentManager.Common.Diagnostics;
@@ -49,49 +47,6 @@ namespace OpenDeploymentManager.Deployment
 
                 return ActivityXamlServices.Load(stream, settings);
             }
-        }
-
-        private static void CompileExpressions(Activity activity)
-        {
-            // activityName is the Namespace.Type of the activity that contains the
-            // C# expressions.
-            string activityName = activity.GetType().ToString();
-
-            // Split activityName into Namespace and Type.Append _CompiledExpressionRoot to the type name
-            // to represent the new type that represents the compiled expressions.
-            // Take everything after the last . for the type name.
-            string activityType = activityName.Split('.').Last() + "_CompiledExpressionRoot";
-
-            // Take everything before the last . for the namespace.
-            string activityNamespace = string.Join(".", activityName.Split('.').Reverse().Skip(1).Reverse());
-
-            // Create a TextExpressionCompilerSettings.
-            var settings = new TextExpressionCompilerSettings
-            {
-                Activity = activity,
-                Language = "C#",
-                ActivityName = activityType,
-                ActivityNamespace = activityNamespace,
-                RootNamespace = null,
-                GenerateAsPartialClass = false,
-                AlwaysGenerateSource = true,
-                ForImplementation = false
-            };
-
-            // Compile the C# expression.
-            TextExpressionCompilerResults results = new TextExpressionCompiler(settings).Compile();
-
-            // Any compilation errors are contained in the CompilerMessages.
-            if (results.HasErrors)
-            {
-                throw new ExpressionCompilationFailedException("Compilation failed.");
-            }
-
-            // Create an instance of the new compiled expression type.
-            var compiledExpressionRoot = Activator.CreateInstance(results.ResultType, new object[] { activity }) as ICompiledExpressionRoot;
-
-            // Attach it to the activity.
-            CompiledExpressionInvoker.SetCompiledExpressionRoot(activity, compiledExpressionRoot);
         }
     }
 }
